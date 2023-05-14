8d294b8dccc1b0bafb2dc98598fc4bbec031f605
Stefano <stdemicheli@protonmail.com>
2023-05-14 10:19:23 +0200

    Implements get account init code.

--- src/account/base_account.rs
+++ src/account/base_account.rs
@@ -1,19 +1,25 @@
 use async_trait::async_trait;
-use ethers::{providers::{Middleware, JsonRpcClient, Provider}, types::U256};
+use std::{error::Error, fmt::Debug};
+use ethers::{providers::{Middleware, JsonRpcClient, Provider}, types::{U256, NameOrAddress, Bytes}};
 
 #[async_trait]
-pub trait BaseAccount {
+pub trait BaseAccount: Sync + Send + Debug {
 
+    type Error: Sync + Send + Error + FromErr<<Self::Inner as Middleware>::Error>;
     type Provider: JsonRpcClient;
     type Inner: Middleware<Provider = Self::Provider>;
 
     fn inner(&self) -> &Self::Inner;
+
+    fn get_account_address(&self) -> NameOrAddress;
     
     fn provider(&self) -> &Provider<Self::Provider> {
         self.inner().provider()
     }
 
-    async fn get_account_init_code(&self) -> Result<Vec<u8>, Box<dyn std::error::Error>>;
+    async fn get_account_init_code(&self) -> Result<Bytes, Self::Error> {
+        self.inner().get_code(self.get_account_address(), None).await.map_err(FromErr::from)
+    }
 
     // TODO: Could also make it sync and have a initialize method on BaseAccount
     async fn get_nonce(&self) -> Result<U256, Box<dyn std::error::Error>>;
@@ -21,4 +27,8 @@ pub trait BaseAccount {
     async fn encode_execute(&self) -> Result<Vec<u8>, Box<dyn std::error::Error>>;
 
     async fn sign_user_op_hash(&self) -> Result<(), Box<dyn std::error::Error>>;
+}
+
+pub trait FromErr<T> {
+    fn from(src: T) -> Self;
 }
