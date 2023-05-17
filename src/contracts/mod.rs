be704438ae4aa71e1d3c16ac55a54143b9d79067
Stefano <stdemicheli@protonmail.com>
2023-05-17 06:26:25 +0200

    Implements from for contract user op.

--- src/contracts/mod.rs
+++ src/contracts/mod.rs
@@ -1,5 +1,30 @@
+use crate::types::user_operation::UserOperationRequest;
+use ethers::types::{Address, Bytes, U256};
+
 mod entry_point;
 pub use entry_point::*;
 
 mod simple_account_factory;
 pub use simple_account_factory::*;
+
+impl From<UserOperationRequest> for UserOperation {
+    fn from(request: UserOperationRequest) -> Self {
+        Self {
+            sender: request.sender.unwrap_or_else(|| Address::zero()),
+            nonce: request.nonce.unwrap_or_else(|| U256::zero()),
+            init_code: request.init_code.unwrap_or_else(|| Bytes::new()),
+            call_data: request.call_data.unwrap_or_else(|| Bytes::new()),
+            call_gas_limit: request.call_gas_limit.unwrap_or_else(|| U256::zero()),
+            verification_gas_limit: request
+                .verification_gas_limit
+                .unwrap_or_else(|| U256::zero()),
+            pre_verification_gas: request.pre_verification_gas.unwrap_or_else(|| U256::zero()),
+            max_fee_per_gas: request.max_fee_per_gas.unwrap_or_else(|| U256::zero()),
+            max_priority_fee_per_gas: request
+                .max_priority_fee_per_gas
+                .unwrap_or_else(|| U256::zero()),
+            paymaster_and_data: request.paymaster_and_data.unwrap_or_else(|| Bytes::new()),
+            signature: request.signature.unwrap_or_else(|| Bytes::new()),
+        }
+    }
+}
