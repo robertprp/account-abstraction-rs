7a37c8c31d4c3f049f175287e4c035df448289e1
Stefano <stdemicheli@protonmail.com>
2023-05-17 08:03:49 +0200

    Implements calc pre verification gas.

--- src/account/utils.rs
+++ src/account/utils.rs
@@ -31,6 +31,36 @@ pub fn get_user_op_hash(
     keccak256(hash_input.encode())
 }
 
+pub fn calc_pre_verification_gas(user_op: UserOperation, overheads: Option<GasOverheads>) -> U256 {
+    let overheads = overheads.unwrap_or_else(|| GasOverheads::default());
+    let dummy_signature = vec![1; 65];
+    let mut user_op = user_op;
+
+    // Set dummy values in case the UserOp is incomplete.
+    user_op.pre_verification_gas = U256::from(21000);
+    user_op.signature = Bytes::from(dummy_signature);
+
+    let packed = pack_user_op(user_op, true);
+
+    let call_data_cost = packed
+        .iter()
+        .map(|&x| {
+            if x == 0 {
+                overheads.zero_byte
+            } else {
+                overheads.non_zero_byte
+            }
+        })
+        .fold(U256::zero(), |acc, x| acc + x);
+
+    let ret = call_data_cost
+        + overheads.fixed / overheads.bundle_size
+        + overheads.per_user_op
+        + overheads.per_user_op_word * U256::from(packed.len());
+
+    ret
+}
+
 #[derive(
     Clone,
     ::ethers::contract::EthAbiType,
@@ -74,6 +104,37 @@ impl From<UserOperation> for UserOperationWithoutSignature {
             max_fee_per_gas: op.max_fee_per_gas,
             max_priority_fee_per_gas: op.max_priority_fee_per_gas,
             paymaster_and_data: keccak256(op.paymaster_and_data),
+        }
+    }
+}
+
+pub struct GasOverheads {
+    /// Fixed overhead for the entire handleOp bundle.
+    pub fixed: U256,
+    /// Per userOp overhead, added on top of the above fixed per-bundle.
+    pub per_user_op: U256,
+    /// Overhead for userOp word (32 bytes) block.
+    pub per_user_op_word: U256,
+    /// Zero byte cost, for calldata gas cost calculations.
+    pub zero_byte: U256,
+    /// Non-zero byte cost, for calldata gas cost calculations.
+    pub non_zero_byte: U256,
+    /// Expected bundle size, to split per-bundle overhead between all ops.
+    pub bundle_size: U256,
+    /// Expected length of the userOp signature.
+    pub sig_size: U256,
+}
+
+impl GasOverheads {
+    fn default() -> Self {
+        GasOverheads {
+            fixed: U256::from(21000),
+            per_user_op: U256::from(18300),
+            per_user_op_word: U256::from(4),
+            zero_byte: U256::from(4),
+            non_zero_byte: U256::from(16),
+            bundle_size: U256::from(1),
+            sig_size: U256::from(65),
         }
     }
 }
