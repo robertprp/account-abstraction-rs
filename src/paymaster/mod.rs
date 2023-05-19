1386ac9e2898d047abe98eca1d65257b0fbdb714
Stefano <stdemicheli@protonmail.com>
2023-05-19 11:08:23 +0200

    Removes paymaster.rs

--- src/paymaster/mod.rs
+++ src/paymaster/mod.rs
@@ -1,2 +1,17 @@
-mod paymaster;
-pub use paymaster::*;
+use crate::contracts::UserOperation;
+
+use async_trait::async_trait;
+use ethers::types::Bytes;
+use thiserror::Error;
+
+#[async_trait]
+pub trait Paymaster: Send + Sync {
+    async fn get_paymaster_and_data(&self, user_op: UserOperation)
+        -> Result<Bytes, PaymasterError>;
+}
+
+#[derive(Debug, Error)]
+pub enum PaymasterError {
+    #[error("custom error: {0}")]
+    Custom(String),
+}
