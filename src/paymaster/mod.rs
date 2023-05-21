38439804b4e68beb043d1703a95f95e44c4e8d9e
Stefano <stdemicheli@protonmail.com>
2023-05-21 14:36:16 +0200

    Adds debug.

--- src/paymaster/mod.rs
+++ src/paymaster/mod.rs
@@ -3,9 +3,10 @@ use crate::contracts::UserOperation;
 use async_trait::async_trait;
 use ethers::types::Bytes;
 use thiserror::Error;
+use std::fmt::Debug;
 
 #[async_trait]
-pub trait Paymaster: Send + Sync {
+pub trait Paymaster: Sync + Send + Debug {
     async fn get_paymaster_and_data(&self, user_op: UserOperation)
         -> Result<Bytes, PaymasterError>;
 }
