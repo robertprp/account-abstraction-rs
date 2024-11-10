a922a9e638f3046b11b35beb9c894a4b8ecdf582
Stefano <stdemicheli@protonmail.com>
2024-11-10 19:20:46 +0700

    Implements get user op hash.

--- alloy/src/smart_account/utils.rs
+++ alloy/src/smart_account/utils.rs
@@ -1,6 +1,67 @@
-use alloy::primitives::{Address, Bytes, U256};
+use alloy::{primitives::{keccak256, Address, Bytes, U256}, sol, sol_types::SolValue};
 
 use crate::types::{PackedUserOperation, UserOperation};
+
+pub fn get_user_op_hash(
+    user_op: &UserOperation,
+    entry_point: Address,
+    chain_id: U256,
+) -> [u8; 32] {
+    // First get the hash of the encoded user operation
+    let user_op_encoded = encode_user_op(user_op, true);
+    let user_op_hash = keccak256(user_op_encoded);
+
+    // Create and encode the data
+    let data = (
+        user_op_hash,
+        entry_point,
+        chain_id,
+    );
+
+    // Return the keccak256 hash of the encoded data
+    keccak256(data.abi_encode()).into()
+}
+
+pub fn encode_user_op(user_op: &UserOperation, for_signature: bool) -> Bytes {
+    let packed_user_op = pack_user_op(user_op);
+    
+    if for_signature {
+        let encoded = UserOpForSignature {
+            sender: packed_user_op.sender,
+            nonce: packed_user_op.nonce,
+            initCodeHash: keccak256(packed_user_op.initCode),
+            callDataHash: keccak256(packed_user_op.callData),
+            accountGasLimits: packed_user_op.accountGasLimits,
+            preVerificationGas: packed_user_op.preVerificationGas,
+            gasFees: packed_user_op.gasFees,
+            paymasterAndDataHash: keccak256(packed_user_op.paymasterAndData),
+        }.abi_encode();
+
+        encoded.into()
+    } else {
+        let encoded = UserOpForGas {
+            sender: packed_user_op.sender,
+            nonce: packed_user_op.nonce,
+            initCode: packed_user_op.initCode,
+            callData: packed_user_op.callData,
+            accountGasLimits: packed_user_op.accountGasLimits,
+            preVerificationGas: packed_user_op.preVerificationGas,
+            gasFees: packed_user_op.gasFees,
+            paymasterAndData: packed_user_op.paymasterAndData,
+            signature: packed_user_op.signature,
+        }.abi_encode();
+
+        encoded.into()
+    }
+}
+
+// pub fn encode_user_op(user_op: &UserOperation, for_signature: bool) -> Bytes {
+//     let packed_user_op = pack_user_op(user_op);
+    
+//     let encoded_packed_user_op: Bytes = packed_user_op.abi_encode().into();
+
+//     encoded_packed_user_op
+// }
 
 pub fn pack_user_op(user_op: &UserOperation) -> PackedUserOperation {
     // Pack gas limits
@@ -40,7 +101,7 @@ pub fn pack_user_op(user_op: &UserOperation) -> PackedUserOperation {
     }
 }
 
-pub fn pack_account_gas_limits(verification_gas_limit: U256, call_gas_limit: U256) -> [u8; 32] {
+fn pack_account_gas_limits(verification_gas_limit: U256, call_gas_limit: U256) -> [u8; 32] {
     let mut result = [0u8; 32];
     
     // Convert verification_gas_limit to bytes and pad to 16 bytes
@@ -54,7 +115,7 @@ pub fn pack_account_gas_limits(verification_gas_limit: U256, call_gas_limit: U25
     result
 }
 
-pub fn pack_paymaster_data(
+fn pack_paymaster_data(
     paymaster: Address,
     paymaster_verification_gas_limit: U256,
     post_op_gas_limit: U256,
@@ -79,10 +140,67 @@ pub fn pack_paymaster_data(
     Bytes::from(result)
 }
 
+sol! {
+    struct UserOpForSignature {
+        address sender;
+        uint256 nonce;
+        bytes32 initCodeHash;
+        bytes32 callDataHash;
+        bytes32 accountGasLimits;
+        uint256 preVerificationGas;
+        bytes32 gasFees;
+        bytes32 paymasterAndDataHash;
+    }
+}
+
+sol! {
+    struct UserOpForGas {
+        address sender;
+        uint256 nonce;
+        bytes initCode;
+        bytes callData;
+        bytes32 accountGasLimits;
+        uint256 preVerificationGas;
+        bytes32 gasFees;
+        bytes paymasterAndData;
+        bytes signature;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::str::FromStr;
+
+    #[test]
+    fn test_get_user_op_hash() {
+        // Create a test UserOperation
+        let user_op = UserOperation {
+            sender: Address::from_slice(&[1u8; 20]),
+            nonce: U256::from(1),
+            init_code: Bytes::from(vec![2u8; 32]),
+            call_data: Bytes::from(vec![3u8; 32]),
+            verification_gas_limit: U256::from(100000),
+            call_gas_limit: U256::from(200000),
+            pre_verification_gas: U256::from(300000),
+            max_fee_per_gas: U256::from(400000),
+            max_priority_fee_per_gas: U256::from(500000),
+            paymaster: Address::from_slice(&[4u8; 20]),
+            paymaster_verification_gas_limit: U256::from(600000),
+            paymaster_post_op_gas_limit: U256::from(700000),
+            paymaster_data: Bytes::from(vec![5u8; 32]),
+            signature: Bytes::from(vec![6u8; 65]),
+        };
+
+        // Test values
+        let entry_point = Address::from_str("0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789").unwrap();
+        let chain_id = U256::from(1);
+
+        let hash = get_user_op_hash(&user_op, entry_point, chain_id);
+        
+        // The hash should be 32 bytes
+        assert_eq!(hash.len(), 32);
+    }
 
     #[test]
     fn test_pack_account_gas_limits() {
