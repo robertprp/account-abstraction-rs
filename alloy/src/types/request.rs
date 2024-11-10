63865a851d2d4c9c7aa04bcc9be78e7b064b973e
Stefano <stdemicheli@protonmail.com>
2024-11-10 11:53:23 +0700

    Adds 0.7 userop types.

---
+++ alloy/src/types/request.rs
@@ -0,0 +1,282 @@
+use alloy::primitives::{Address, Bytes, B256, U256};
+use serde::Serialize;
+
+#[derive(Clone, Default, Serialize, PartialEq, Eq, Debug)]
+pub struct UserOperationRequest {
+    /// The account making the operation
+    #[serde(skip_serializing_if = "Option::is_none", default)]
+    pub sender: Option<Address>,
+
+    /// Anti-replay parameter
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub nonce: Option<U256>,
+
+    /// Account factory, only for new accounts
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub factory: Option<Address>,
+
+    /// Data for account factory
+    #[serde(
+        rename = "factoryData",
+        default,
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub factory_data: Option<Bytes>,
+
+    /// The data to pass to the sender during the main execution call
+    #[serde(rename = "callData", default, skip_serializing_if = "Option::is_none")]
+    pub call_data: Option<Bytes>,
+
+    /// The amount of gas to allocate the main execution call
+    #[serde(
+        rename = "callGasLimit",
+        default,
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub call_gas_limit: Option<U256>,
+
+    /// The amount of gas to allocate for the verification step
+    #[serde(
+        rename = "verificationGasLimit",
+        default,
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub verification_gas_limit: Option<U256>,
+
+    /// Extra gas to pay the bundler
+    #[serde(
+        rename = "preVerificationGas",
+        default,
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub pre_verification_gas: Option<U256>,
+
+    /// Maximum fee per gas (similar to EIP-1559 max_fee_per_gas)
+    #[serde(
+        rename = "maxFeePerGas",
+        default,
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub max_fee_per_gas: Option<U256>,
+
+    /// Maximum priority fee per gas (similar to EIP-1559 max_priority_fee_per_gas)
+    #[serde(
+        rename = "maxPriorityFeePerGas",
+        default,
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub max_priority_fee_per_gas: Option<U256>,
+
+    /// Address of paymaster contract
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub paymaster: Option<Address>,
+
+    /// Gas limit for paymaster validation
+    #[serde(
+        rename = "paymasterVerificationGasLimit",
+        default,
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub paymaster_verification_gas_limit: Option<U256>,
+
+    /// Gas limit for paymaster post-operation
+    #[serde(
+        rename = "paymasterPostOpGasLimit",
+        default,
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub paymaster_post_op_gas_limit: Option<U256>,
+
+    /// Data for paymaster
+    #[serde(
+        rename = "paymasterData",
+        default,
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub paymaster_data: Option<Bytes>,
+
+    /// Data passed into the account to verify authorization
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub signature: Option<Bytes>,
+
+    #[serde(skip_serializing)]
+    pub call: Option<AccountCall>,
+}
+
+impl UserOperationRequest {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn with_defaults(self) -> Self {
+        Self {
+            sender: Some(self.sender.unwrap_or_else(|| Address::ZERO)),
+            nonce: Some(self.nonce.unwrap_or_else(|| U256::ZERO)),
+            factory: self.factory,
+            factory_data: self.factory_data,
+            call_data: Some(self.call_data.unwrap_or_else(|| Bytes::from(vec![0u8; 0]))),
+            call_gas_limit: Some(self.call_gas_limit.unwrap_or_else(|| U256::ZERO)),
+            verification_gas_limit: Some(self.verification_gas_limit.unwrap_or_else(|| U256::ZERO)),
+            pre_verification_gas: Some(self.pre_verification_gas.unwrap_or_else(|| U256::ZERO)),
+            max_fee_per_gas: Some(self.max_fee_per_gas.unwrap_or_else(|| U256::ZERO)),
+            max_priority_fee_per_gas: Some(self.max_priority_fee_per_gas.unwrap_or_else(|| U256::ZERO)),
+            paymaster: self.paymaster,
+            paymaster_verification_gas_limit: self.paymaster_verification_gas_limit,
+            paymaster_post_op_gas_limit: self.paymaster_post_op_gas_limit,
+            paymaster_data: self.paymaster_data,
+            signature: Some(self.signature.unwrap_or_else(|| "0xb6905c3cc524616247f41b6de20bced7d4437a6513a5cf1c90ab6a28415eb6993e1236443130bce47d1580ab289bcc8f32270acb4ae7e46a17fe158f473fd4991c".parse().unwrap())),
+            call: self.call,
+        }
+    }
+
+    #[must_use]
+    pub fn sender<T: Into<Address>>(mut self, sender: T) -> Self {
+        self.sender = Some(sender.into());
+        self
+    }
+
+    #[must_use]
+    pub fn nonce<T: Into<U256>>(mut self, nonce: T) -> Self {
+        self.nonce = Some(nonce.into());
+        self
+    }
+
+    pub fn set_nonce<T: Into<U256>>(&mut self, nonce: T) -> &mut Self {
+        self.nonce = Some(nonce.into());
+        self
+    }
+
+    #[must_use]
+    pub fn factory<T: Into<Address>>(mut self, factory: T) -> Self {
+        self.factory = Some(factory.into());
+        self
+    }
+
+    #[must_use]
+    pub fn factory_data<T: Into<Bytes>>(mut self, factory_data: T) -> Self {
+        self.factory_data = Some(factory_data.into());
+        self
+    }
+
+    #[must_use]
+    pub fn call_data<T: Into<Bytes>>(mut self, call_data: T) -> Self {
+        self.call_data = Some(call_data.into());
+        self
+    }
+
+    #[must_use]
+    pub fn call_gas_limit<T: Into<U256>>(mut self, call_gas_limit: T) -> Self {
+        self.call_gas_limit = Some(call_gas_limit.into());
+        self
+    }
+
+    #[must_use]
+    pub fn verification_gas_limit<T: Into<U256>>(mut self, verification_gas_limit: T) -> Self {
+        self.verification_gas_limit = Some(verification_gas_limit.into());
+        self
+    }
+
+    #[must_use]
+    pub fn pre_verification_gas<T: Into<U256>>(mut self, pre_verification_gas: T) -> Self {
+        self.pre_verification_gas = Some(pre_verification_gas.into());
+        self
+    }
+
+    #[must_use]
+    pub fn max_fee_per_gas<T: Into<U256>>(mut self, max_fee_per_gas: T) -> Self {
+        self.max_fee_per_gas = Some(max_fee_per_gas.into());
+        self
+    }
+
+    #[must_use]
+    pub fn max_priority_fee_per_gas<T: Into<U256>>(mut self, max_priority_fee_per_gas: T) -> Self {
+        self.max_priority_fee_per_gas = Some(max_priority_fee_per_gas.into());
+        self
+    }
+
+    #[must_use]
+    pub fn paymaster<T: Into<Address>>(mut self, paymaster: T) -> Self {
+        self.paymaster = Some(paymaster.into());
+        self
+    }
+
+    #[must_use]
+    pub fn paymaster_verification_gas_limit<T: Into<U256>>(mut self, gas_limit: T) -> Self {
+        self.paymaster_verification_gas_limit = Some(gas_limit.into());
+        self
+    }
+
+    #[must_use]
+    pub fn paymaster_post_op_gas_limit<T: Into<U256>>(mut self, gas_limit: T) -> Self {
+        self.paymaster_post_op_gas_limit = Some(gas_limit.into());
+        self
+    }
+
+    #[must_use]
+    pub fn paymaster_data<T: Into<Bytes>>(mut self, data: T) -> Self {
+        self.paymaster_data = Some(data.into());
+        self
+    }
+
+    #[must_use]
+    pub fn signature<T: Into<Bytes>>(mut self, signature: T) -> Self {
+        self.signature = Some(signature.into());
+        self
+    }
+
+    #[must_use]
+    pub fn call<T: Into<AccountCall>>(mut self, call: T) -> Self {
+        self.call = Some(call.into());
+        self
+    }
+
+    #[must_use]
+    pub fn execute<T: Into<Address>, V: Into<U256>, D: Into<Bytes>>(
+        mut self,
+        target: T,
+        value: V,
+        data: D,
+    ) -> Self {
+        self.call = Some(AccountCall::Execute(ExecuteCall::new(
+            target.into(),
+            value.into(),
+            data.into(),
+        )));
+        self
+    }
+
+    #[must_use]
+    pub fn execute_batch(mut self, calls: Vec<ExecuteCall>) -> Self {
+        self.call = Some(AccountCall::ExecuteBatch(calls));
+        self
+    }
+}
+
+pub type UserOpHash = B256;
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum AccountCall {
+    Execute(ExecuteCall),
+    ExecuteBatch(Vec<ExecuteCall>),
+}
+
+#[derive(Clone, Default, PartialEq, Eq, Debug)]
+pub struct ExecuteCall {
+    pub target: Address,
+    pub value: U256,
+    pub data: Bytes,
+}
+
+impl ExecuteCall {
+    pub fn new<T: Into<Address>, V: Into<U256>, B: Into<Bytes>>(
+        target: T,
+        value: V,
+        data: B,
+    ) -> Self {
+        Self {
+            target: target.into(),
+            value: value.into(),
+            data: data.into(),
+        }
+    }
+}
