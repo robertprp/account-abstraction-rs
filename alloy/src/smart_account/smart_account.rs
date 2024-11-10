69f34c30180fd10646091cbbd6b2e753b9963f87
Stefano <stdemicheli@protonmail.com>
2024-11-10 17:00:58 +0700

    Initial interface definition.

--- alloy/src/smart_account/smart_account.rs
+++ alloy/src/smart_account/smart_account.rs
@@ -1,8 +1,11 @@
 use std::fmt::Debug;
-use alloy::{network::{Ethereum, Network}, providers::Provider, transports::Transport};
+use alloy::{network::{Ethereum, Network}, primitives::{Address, Bytes, U256}, providers::Provider, transports::Transport};
 use async_trait::async_trait;
+use thiserror::Error;
 
-use super::EntryPoint;
+use crate::types::ExecuteCall;
+
+use super::{EntryPoint, EntryPointError, SmartAccountSigner};
 
 #[async_trait]
 pub trait SmartAccount<P: Provider<T, N>, T: Transport + Clone, N: Network = Ethereum>: Sync + Send + Debug {
@@ -12,4 +15,75 @@ pub trait SmartAccount<P: Provider<T, N>, T: Transport + Clone, N: Network = Eth
     fn provider(&self) -> &Self::P;
 
     fn entry_point(&self) -> &Self::EntryPoint;
+
+
+    async fn get_account_address(&self) -> Result<Address, AccountError>;
+
+    async fn get_nonce(&self) -> Result<U256, AccountError> {
+        // TODO: Use cache trait to cache nonce, address, etc. Can also initialize
+
+        let account_address: Address = self.get_account_address().await?;
+
+        self.entry_point()
+            .get_nonce(account_address)
+            .await
+            .map_err(AccountError::EntryPointError)
+    }
+
+    async fn get_init_code(&self) -> Result<Bytes, AccountError>;
+
+    async fn is_account_deployed(&self) -> Result<bool, AccountError>;
+
+    async fn encode_execute(&self, call: ExecuteCall) -> Result<Vec<u8>, AccountError>;
+
+    async fn encode_execute_batch(&self, calls: Vec<ExecuteCall>) -> Result<Vec<u8>, AccountError>;
+
+    async fn sign_user_op_hash<S: SmartAccountSigner>(
+        &self,
+        user_op_hash: [u8; 32],
+        signer: &S,
+    ) -> Result<Bytes, AccountError>;
+
+    async fn get_counterfactual_address(&self) -> Result<Address, AccountError> {
+        let init_code: Bytes = self.get_init_code().await?;
+
+        let address = self.entry_point()
+            .get_sender_address(init_code)
+            .await
+            .map_err(AccountError::EntryPointError)?;
+
+        if address == Address::ZERO {
+            return Err(AccountError::InvalidInitCodeError);
+        }
+
+        Ok(address)
+    }
+}
+
+
+#[derive(Debug, Error)]
+pub enum AccountError {
+    #[error("decode error: {0}")]
+    DecodeError(String),
+
+    #[error("encode error: {0}")]
+    EncodeError(String),
+
+    #[error("revert error: {0}")]
+    RevertError(String),
+
+    #[error("contract error: {0}")]
+    EntryPointError(EntryPointError),
+
+    // #[error("provider error: {0}")]
+    // ProviderError(ProviderError),
+
+    #[error("nonce error")]
+    NonceError,
+
+    #[error("signer error")]
+    SignerError,
+
+    #[error("invalid init code error")]
+    InvalidInitCodeError,
 }
