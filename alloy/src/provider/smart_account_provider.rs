bd0a680d2eb66371824c0f67403c627166d553a9
Stefano <stdemicheli@protonmail.com>
2025-01-01 11:30:47 +0700

    Adds account to provider. Updates tests.

--- alloy/src/provider/smart_account_provider.rs
+++ alloy/src/provider/smart_account_provider.rs
@@ -1,4 +1,5 @@
 use crate::signer::SmartAccountSigner;
+use crate::smart_account::SmartAccount;
 use crate::types::request::{UserOpHash, UserOperation, UserOperationRequest};
 use alloy::providers::RootProvider;
 use alloy::{
@@ -79,32 +80,36 @@ where
 }
 
 #[derive(Clone, Debug)]
-pub struct SmartAccountProvider<P, T, N> {
+pub struct SmartAccountProvider<P, T, N, A> {
     inner: P,
+    account: A,
     _transport: std::marker::PhantomData<T>,
     _network: std::marker::PhantomData<N>,
 }
 
-impl<P, T, N> SmartAccountProvider<P, T, N>
+impl<P, T, N, A> SmartAccountProvider<P, T, N, A>
 where
     P: Provider<T, N>,
     T: Transport + Clone,
     N: Network,
+    A: SmartAccount<P, T, N>,
 {
-    pub fn new(inner: P) -> Self {
+    pub fn new(inner: P, account: A) -> Self {
         Self {
             inner,
+            account,
             _transport: std::marker::PhantomData,
             _network: std::marker::PhantomData,
         }
     }
 }
 
-impl<P, T, N> Provider<T, N> for SmartAccountProvider<P, T, N>
+impl<P, T, N, A> Provider<T, N> for SmartAccountProvider<P, T, N, A>
 where
     P: Provider<T, N>,
     T: Transport + Clone,
     N: Network,
+    A: SmartAccount<P, T, N>,
 {
     fn root(&self) -> &RootProvider<T, N> {
         self.inner.root()
@@ -112,36 +117,136 @@ where
 }
 
 #[async_trait]
-impl<P, T, N> SmartAccountProviderTrait<N, T> for SmartAccountProvider<P, T, N>
+impl<P, T, N, A> SmartAccountProviderTrait<N, T> for SmartAccountProvider<P, T, N, A>
 where
     P: Provider<T, N> + Send + Sync + std::fmt::Debug,
     T: Transport + Clone + Send + Sync + std::fmt::Debug,
     N: Network + Send + Sync,
+    A: SmartAccount<P, T, N>,
     Self: Provider<T, N>,
 {
     type Error = SmartAccountError;
 
     async fn send_user_operation<S>(
         &self,
-        user_op: UserOperationRequest,
+        mut user_op: UserOperationRequest,
         signer: &S,
         entry_point: Address,
     ) -> Result<UserOpHash, Self::Error>
     where
         S: SmartAccountSigner + Send + Sync,
     {
-        let signed_op = self.sign_user_operation(user_op, signer).await?;
+        self.fill_user_operation(&mut user_op).await?;
+
+        if user_op.signature.is_none() {
+            let signature = self.sign_user_operation(user_op.clone(), signer).await?;
+            user_op.signature = Some(signature);
+        }
+
+        let user_operation = user_op.build_user_operation().await.map_err(|e| {
+            SmartAccountError::Provider(format!("Failed to build user operation: {}", e))
+        })?;
+
         self.inner
             .client()
-            .request("eth_sendUserOperation", (signed_op, entry_point))
+            .request("eth_sendUserOperation", (user_operation, entry_point))
             .await
             .map_err(SmartAccountError::from)
     }
 
     async fn fill_user_operation(
         &self,
-        _user_op: &mut UserOperationRequest,
+        user_op: &mut UserOperationRequest,
     ) -> Result<(), Self::Error> {
+        // if let Some(ref call) = user_op.call {
+        //     let call_data: Bytes = match call {
+        //         AccountCall::Execute(execute_call) => {
+        //             let call_data = self
+        //                 .account
+        //                 .encode_execute(execute_call.clone())
+        //                 .await
+        //                 .map_err(|e| SmartAccountError::Provider(format!("Failed to encode execute: {}", e)))?;
+
+        //             call_data.into()
+        //         }
+        //         AccountCall::ExecuteBatch(execute_calls) => {
+        //             let call_data = self
+        //                 .account
+        //                 .encode_execute_batch(execute_calls.clone())
+        //                 .await
+        //                 .map_err(|e| SmartAccountError::Provider(format!("Failed to encode execute batch: {}", e)))?;
+
+        //             call_data.into()
+        //         }
+        //     };
+
+        //     user_op.call_data = Some(call_data);
+        // }
+
+        if user_op.nonce.is_none() {
+            let nonce = self
+                .account
+                .get_nonce()
+                .await
+                .unwrap_or_default();
+            user_op.nonce = Some(nonce);
+        }
+
+        if user_op.sender.is_none() {
+            user_op.sender = Some(
+                self.account
+                    .get_account_address()
+                    .await
+                    .map_err(|e| SmartAccountError::Provider(format!("Failed to get account address: {}", e)))?,
+            );
+        }
+
+        if user_op.factory_data.is_none() {
+            user_op.factory_data = Some(
+                self.account
+                    .get_init_code()
+                    .await
+                    .map_err(|e| SmartAccountError::Provider(format!("Failed to get init code: {}", e)))?,
+            );
+        }
+
+        // If gas parameters are missing, estimate them
+        if user_op.call_gas_limit.is_none()
+            || user_op.verification_gas_limit.is_none()
+            || user_op.pre_verification_gas.is_none()
+        {
+            let gas_estimate = self
+                .estimate_user_operation_gas(
+                    &user_op.clone().with_defaults(),
+                    user_op
+                        .sender
+                        .ok_or_else(|| SmartAccountError::Provider("Missing sender".into()))?,
+                )
+                .await?;
+
+            if user_op.call_gas_limit.is_none() {
+                user_op.call_gas_limit = Some(gas_estimate.call_gas_limit);
+            }
+
+            if user_op.verification_gas_limit.is_none() {
+                user_op.verification_gas_limit = Some(gas_estimate.verification_gas);
+            }
+            
+            if user_op.pre_verification_gas.is_none() {
+                user_op.pre_verification_gas = Some(gas_estimate.pre_verification_gas);
+            }
+            
+            if user_op.paymaster_verification_gas_limit.is_none() && !user_op.paymaster_data.is_none() {
+                user_op.paymaster_verification_gas_limit = Some(gas_estimate.paymaster_verification_gas);
+            }
+        }
+
+        // If paymaster data is missing, try to get it
+        if user_op.paymaster_data.is_none() {
+            let paymaster_data = self.get_paymaster_and_data(user_op.clone()).await?;
+            user_op.paymaster_data = Some(paymaster_data);
+        }
+
         Ok(())
     }
 
@@ -226,10 +331,85 @@ pub enum SmartAccountError {
 mod tests {
     use super::*;
     use alloy::{
-        network::EthereumWallet, providers::ProviderBuilder, signers::local::PrivateKeySigner,
+        network::{Ethereum, EthereumWallet},
+        primitives::{Address, Bytes, ChainId, U256},
+        providers::ProviderBuilder,
+        signers::local::PrivateKeySigner,
+        transports::http::{Client, Http},
     };
     use alloy_node_bindings::Anvil;
     use url::Url;
+    use crate::{entry_point::{EntryPointError, EntryPointTrait}, smart_account::AccountError, types::ExecuteCall};
+
+    #[derive(Debug)]
+    struct MockSmartAccount;
+
+    #[async_trait]
+    impl<P> SmartAccount<P, Http<Client>, Ethereum> for MockSmartAccount 
+    where
+        P: Provider<Http<Client>, Ethereum> + Clone + Debug + Send + Sync,
+    {
+        type P = P;
+        type EntryPoint = MockEntryPoint;
+
+        fn provider(&self) -> &Self::P {
+            unimplemented!()
+        }
+
+        fn entry_point(&self) -> &Self::EntryPoint {
+            unimplemented!()
+        }
+
+        fn chain_id(&self) -> ChainId {
+            84532
+        }
+
+        async fn get_account_address(&self) -> Result<Address, AccountError> {
+            Ok(Address::ZERO)
+        }
+
+        async fn get_init_code(&self) -> Result<Bytes, AccountError> {
+            Ok(Bytes::default())
+        }
+
+        async fn is_account_deployed(&self) -> Result<bool, AccountError> {
+            Ok(false)
+        }
+
+        async fn encode_execute(&self, _call: ExecuteCall) -> Result<Vec<u8>, AccountError> {
+            Ok(vec![])
+        }
+
+        async fn encode_execute_batch(&self, _calls: Vec<ExecuteCall>) -> Result<Vec<u8>, AccountError> {
+            Ok(vec![])
+        }
+
+        async fn sign_user_op_hash<S: SmartAccountSigner>(
+            &self,
+            _user_op_hash: &[u8; 32],
+            _signer: &S,
+        ) -> Result<Bytes, AccountError> {
+            Ok(Bytes::default())
+        }
+    }
+
+    #[derive(Debug)]
+    struct MockEntryPoint;
+
+    #[async_trait]
+    impl EntryPointTrait for MockEntryPoint {
+        fn get_address(&self) -> Address {
+            Address::ZERO
+        }
+
+        async fn get_sender_address(&self, _init_code: Bytes) -> Result<Address, EntryPointError> {
+            Ok(Address::ZERO)
+        }
+
+        async fn get_nonce(&self, _address: Address) -> Result<U256, EntryPointError> {
+            Ok(U256::ZERO)
+        }
+    }
 
     #[tokio::test]
     async fn test_get_accounts() {
@@ -247,12 +427,11 @@ mod tests {
             .wallet(wallet)
             .on_http(rpc_url);
 
-        let smart_account_provider = SmartAccountProvider::new(provider);
+        let account = MockSmartAccount;
+        let smart_account_provider = SmartAccountProvider::new(provider, account);
 
         let block_number = smart_account_provider.get_block_number().await;
-
         assert!(block_number.is_ok());
-        assert!(block_number.unwrap() > 0);
     }
 
     #[tokio::test]
@@ -271,10 +450,10 @@ mod tests {
             .wallet(wallet)
             .on_http(rpc_url);
 
-        let smart_account_provider = SmartAccountProvider::new(provider);
+        let account = MockSmartAccount;
+        let smart_account_provider = SmartAccountProvider::new(provider, account);
 
         let entry_points = smart_account_provider.get_supported_entry_points().await;
-
         assert!(entry_points.is_ok());
     }
 }
